2787402ef7bf0271d81f17c35ecfde25e448e753
mathyasp <mathyastp@gmail.com>
2024-01-16 13:59:44 -0500

    completed step 4

--- scripts.js
+++ scripts.js
@@ -6,6 +6,30 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 const ballRadius = 10;
+const paddleWidth = 75;
+const paddleHeight = 10;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = true;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = true;
+  };
+};
+
+function keyUpHandler(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = false;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = false;
+  };
+};
 
 function drawBall() {
   ctx.beginPath();
@@ -13,11 +37,20 @@ function drawBall() {
   ctx.fillStyle = '#0095DD';
   ctx.fill();
   ctx.closePath();
-}
+};
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath;
+};
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
 
   if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
     dy = -dy;
@@ -29,6 +62,13 @@ function draw() {
 
   x += dx;
   y += dy;
+
+  if (rightPressed) {
+    paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+  } else if (leftPressed) {
+    paddleX = Math.max(paddleX - 7, 0);
+  };
 };
+
 
 setInterval(draw, 10);
